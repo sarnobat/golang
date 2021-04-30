fba460adef28863b2bd2d047f91ef0163af8666d
Sridhar Sarnobat <ss401533@gmail.com>
2021-04-29 19:57:04 -0700

    Minor changes (untested)

--- 1_helloworld/run.sh
+++ 1_helloworld/run.sh
@@ -1,7 +1,8 @@
 go run helloworld.go
 
-# static linking?
-go tool compile helloworld.go
+# static linking (2021)
+go build helloworld.go
 
-# static linking
-env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o golang_tutorial
+# static linking (2020) - not necessary anymore apparently
+#go tool compile helloworld.go
+#env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o golang_tutorial
