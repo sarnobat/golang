7f302db438abf4d9ef91163f28cab57c0db0ecdc
Sridhar Sarnobat <ss401533@gmail.com>
2021-05-03 19:07:56 -0700

    Minor changes (untested)

--- 3_gedcom/gedcom_indent.go
+++ 3_gedcom/gedcom_indent.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
-	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 // Thanks to this page for the original code:
 // https://ebixio.com/blog/2012/03/05/editing-gedcom-files-with-vim
 func main() {
-	
+
 	in := bufio.NewReader(os.Stdin)
 	for {
 		s, err := in.ReadString('\n')
@@ -22,12 +22,11 @@ func main() {
 				log.Fatal(err)
 			}
 			break
-		}		
-		trimmed := strings.TrimSpace(s);
-		level,err := strconv.Atoi(strings.Split(trimmed," ")[0])
-        fmt.Print(strings.Repeat("\t",level))
-        fmt.Print(trimmed)
-        fmt.Print("\n")
+		}
+		trimmed := strings.TrimSpace(s)
+		level, err := strconv.Atoi(strings.Split(trimmed, " ")[0])
+		fmt.Print(strings.Repeat("\t", level))
+		fmt.Print(trimmed)
+		fmt.Print("\n")
 	}
 }
-
