f1058e1c43ded0ebb17f44a0f71613e438b3abfa
Sridhar Sarnobat <ss401533@gmail.com>
2021-05-31 21:38:17 -0700

    Minor changes (untested)

--- 6_mwk2fs/run.sh
+++ 6_mwk2fs/run.sh
@@ -6,6 +6,7 @@ mv main tree2fs.linux
 
 env GOOS=darwin GOARCH=amd64 go build main.go
 cp main tree2fs.osx
+mv main tree2fs
 
 #sort du_jpg_only_reduced_md5sum.txt  | main | sort -n | tee /tmp/out.txt
 #echo "cat /tmp/out.txt | tail -500 "
