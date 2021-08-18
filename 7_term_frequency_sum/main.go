a14ddda5d17c91bd9123ca57d80cfd248945fe2a
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-08-17 22:25:22 -0700

    Minor changes (untested)

--- 7_term_frequency_sum/main.go
+++ 7_term_frequency_sum/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"regexp"
 	"github.com/pborman/getopt"
 	"io"
 	"log"
 	"os"
+	"regexp"
 )
+
+func pinger(c chan string) {
+	for i := 0; i < 3; i++ {
+		c <- "ping"
+	}
+	close(c)
+}
 
 func main() {
 	optName := getopt.StringLong("name", 'n', "Sridhar", "Your name")
@@ -21,6 +28,31 @@ func main() {
 	}
 	fmt.Println("name = " + *optName)
 
+	documentFrequenciesMap := make(map[string]int)
+	//     messages := make(chan string)
+	//
+	// 	go func() {
+	// 	    msg := <-messages
+	// 		fmt.Println(msg)
+	// 	}()
+	//
+	// 	messages <- "ping"
+
+	var c chan string = make(chan string)
+
+	go func(c chan string) {
+		for i := 0; i < 3; i++ {
+			c <- "ping"
+		}
+		close(c)
+	}(c)
+
+	for msg := range c {
+		fmt.Println(msg)
+	}
+
+	os.Exit(0)
+
 	in := bufio.NewReader(os.Stdin)
 	for {
 		line, err := in.ReadString('\n')
@@ -31,7 +63,7 @@ func main() {
 			break
 		}
 		_ = line
-		fmt.Print("[debug] line: " + line)
+		println("[debug] line: " + line)
 
 		regex := "^\\s*([0-9]+)*\\s*DOCUMENT_FREQUENCY_TOTAL..(.*)\n"
 		r := regexp.MustCompile(regex)
@@ -39,14 +71,22 @@ func main() {
 
 		if len(elem) == 0 {
 			// no match
+			// 			messages <- "foo"
 			continue
-		}
+		} else {
 
-		// elem[0] is the entire line
-		for i := 1; i < len(elem); i++ {
-			fmt.Print(elem[i])
-			fmt.Println()
+			// 			messages <- "bar"
+
+			documentFrequenciesMap[elem[2]] += 1
+			// elem[0] is the entire line
+			for i := 1; i < len(elem); i++ {
+				fmt.Print(elem[i])
+				fmt.Println()
+			}
 		}
 	}
+
 	fmt.Println()
+	fmt.Println("map:", documentFrequenciesMap)
+
 }
