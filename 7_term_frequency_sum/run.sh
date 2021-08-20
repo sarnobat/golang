eae1a0143090c7af833d5f875c539c9643d47dbb
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-08-19 23:26:52 -0700

    Minor changes (untested)

--- 7_term_frequency_sum/run.sh
+++ 7_term_frequency_sum/run.sh
@@ -1,3 +1,4 @@
 #echo "      5 DOCUMENT_FREQUENCY_TOTAL: component" | go run main.go
 
-cat /Volumes/git/github/mahout/shell_simplification/cat_with_filename.50.out | go run main.go 2> /dev/null
+#cat /Volumes/git/github/mahout/shell_simplification/cat_with_filename.50.out | go run main.go 2> /dev/null
+cat cat_with_filename.full.out | go run main.go  2> /dev/null | sort -n | grep -v '\t0' | grep -v '\t1' | grep -v '2.0' | uniq | edit -
