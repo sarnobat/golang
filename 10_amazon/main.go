0c0de18e1b4cb6e4145141d6d3b3940d8992e2ce
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-10-02 13:18:38 -0700

    Cron daily autocommit

---
+++ 10_amazon/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+
+    paapi5 "github.com/spiegel-im-spiegel/pa-api"
+    "github.com/spiegel-im-spiegel/pa-api/entity"
+    "github.com/spiegel-im-spiegel/pa-api/query"
+)
+
+func main() {
+    //Create client
+    client := paapi5.New(
+        paapi5.WithMarketplace(paapi5.LocaleJapan),
+    ).CreateClient(
+        "mytag-20",
+        "AKIAIOSFODNN7EXAMPLE",
+        "1234567890",
+    )
+
+    //Make query
+    q := query.NewGetItems(
+        client.Marketplace(),
+        client.PartnerTag(),
+        client.PartnerType(),
+    ).ASINs([]string{"B08TQSZYHN"}).EnableImages().EnableItemInfo().EnableParentASIN()
+
+    //Requet and response
+    body, err := client.Request(q)
+    if err != nil {
+        fmt.Printf("%+v\n", err)
+        return
+    }
+    //io.Copy(os.Stdout, bytes.NewReader(body))
+
+    //Decode JSON
+    res, err := entity.DecodeResponse(body)
+    if err != nil {
+        fmt.Printf("%+v\n", err)
+        return
+    }
+    fmt.Println(res.String())
+}
+
--- 7_term_frequency_sum/run.sh
+++ 7_term_frequency_sum/run.sh
@@ -1,5 +1,7 @@
 #echo "      5 DOCUMENT_FREQUENCY_TOTAL: component" | go run main.go
 
+# TODO: this isn't quite correct - they should be percentages, not frequencies
+
 #cat /Volumes/git/github/mahout/shell_simplification/cat_with_filename.50.out | go run main.go 2> /dev/null
 cat cat_with_filename.full.out | go run main.go  2> /dev/null | sort -n | grep -v '\t0.0'  | uniq | tee keywords.out | edit -
 cat keywords.out | grep -v http | perl -pe 's{phase.3..([^\s]+)\s+(.+)\s+(.+)$}{$3\t| $2 |\t$1\n}g'  | sort -n | tee word_to_file_mappings.txt
