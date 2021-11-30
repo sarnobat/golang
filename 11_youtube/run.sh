c27219989eb5e438291118cfe20b6591f860e20e
Sridhar Sarnobat.deprecated <sarnobat@Sridhars-MacBook-Air.local>
2021-11-29 23:14:29 -0800

    Minor changes (untested)

--- 11_youtube/run.sh
+++ 11_youtube/run.sh
@@ -5,6 +5,9 @@ mv playlists playlists.linux
 env GOOS=darwin GOARCH=amd64 go build playlists.go
 cp playlists playlists.osx
 
-# Note: watch later playlist is not supported anymore by the API. Back to greasemonkey
-go run playlists.go --clientSecret /Users/sarnobat/trash/client_secret_803470544206-ni4anh43sh69athsll8s32qkb5dn0des.apps.googleusercontent.com.json
-playlists --clientSecret /Users/sarnobat/trash/client_secret_803470544206-lvipsulouuemapfptubh5uq2qop22fj5.apps.googleusercontent.com.json | tee playlists.out.txt
+# Note: watch later playlist is not supported anymore by the API. Back to greasemonkey :(
+
+# TODO - try to do this with a Dockerfile for experience, and to see what is still not automatic
+# minimal example
+go run playlists.go --clientSecret client_secret_803470544206-ni4anh43sh69athsll8s32qkb5dn0des.apps.googleusercontent.com.json
+playlists --clientSecret go run playlists.go --clientSecret client_secret_803470544206-ni4anh43sh69athsll8s32qkb5dn0des.apps.googleusercontent.com.json
