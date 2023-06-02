7d8f69830bd11589d38cac1addc97c1dfe254ab4
Sridhar Sarnobat <srsarnob@cisco.com>
2023-06-01 21:20:22 -0400

    Minor changes (untested)

--- 13_isempty/main.go
+++ 13_isempty/main.go
@@ -11,7 +11,7 @@
 //
 // DATE
 //
-//	2022 Jan 20
+//	2023 Jun 01
 //
 
 //-----------------------------------------------------------------------------------------
@@ -26,10 +26,8 @@ import (
 	"io"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"regexp"
 	"strings"
+    "io/ioutil"
 )
 
 var counts = make(map[string]int)
@@ -40,15 +38,18 @@ func main() {
 	///
 	/// 5) CLI options
 	///
-	optName := getopt.StringLong("name", 'n', "", "Your name")
+// 	optName := getopt.StringLong("invert", 'v', "", "Invert match")
 	optHelp := getopt.BoolLong("help", 0, "Help")
+	optInvert := getopt.BoolLong("invert", 'v', "Invert match")
 	getopt.Parse()
 
 	if *optHelp {
 		getopt.Usage()
 		os.Exit(0)
 	}
-	fmt.Println("Hello " + *optName)
+	
+	result := fmt.Sprintf("invert=%t", *optInvert)
+	fmt.Println(result)
 
 	///
 	/// 1) Loop over stdin
@@ -75,78 +76,29 @@ func main() {
 
 		if file, err := os.Stat(p); !os.IsNotExist(err) {
 
-			// check if it's a file or a dir
 			switch i, err := os.Stat(p); {
 			case err != nil:
 				fmt.Println(err)
 			case i.IsDir():
-				fmt.Println("	", p, "is a directory")
+				files, err := ioutil.ReadDir(p)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				if len(files) == 0 {
+					fmt.Fprintf(os.Stderr, "[empty dir]" + p + "\n")
+					fmt.Println(p)
+				} else {
+					fmt.Fprintf(os.Stderr, "[not empty dir]" + p + "\n")
+				}
+
 			default:
-				fmt.Println("	", p, "is a file")
-			}
-			// get absolute path
-			abs, _ := filepath.Abs(p)
-			fmt.Println("	absolute: " + abs)
-
-			///
-			/// 6) Call a shell program instead
-			///
-			cmd := exec.Command("dirname", p)
-			out, err := cmd.CombinedOutput()
-			if err != nil {
-				log.Fatal(err, out)
-			}
-
-			cmd2 := exec.Command("basename", p)
-			out2, err2 := cmd2.CombinedOutput()
-			if err2 != nil {
-				log.Fatal(err2)
-			}
-			dirname := strings.Trim(string(out2), "\n")
-			//			fmt.Println("	DIRNAME: ", dirname)
-
-			///
-			/// 5) dictionary
-			///
-			_, exist := counts[dirname]
-			if !exist {
-				counts[dirname] = 1
-			} else {
-				counts[dirname]++
-			}
-
-			// We don't need to check if it's a folder to recurse into. Stdin will ensure that
-			// we recurse.
-			if counts[dirname] <= 5 {
-				fmt.Println("", p, "\n")
-			}
-
-		} else {
-			///
-			/// 1) Print to stdout
-			///
-			if file == nil {
-				fmt.Print("from go: ", p, "\n")
-			}
-
-			///
-			/// 2) Regex capture groups extracted and read separately (not used directly
-			/// in a substitution) will cover more scenarios.
-			///
-			regex := "^\\s*([0-9]+)*\\s*DOCUMENT_FREQUENCY_TOTAL..(.*)\n"
-			r := regexp.MustCompile(regex)
-			line := s
-			elem := r.FindStringSubmatch(line)
-
-			if len(elem) == 0 {
-				// no match
-				continue
-			}
-
-			// elem[0] is the entire line
-			for i := 1; i < len(elem); i++ {
-				fmt.Print(elem[i])
-				fmt.Println()
+				if file.Size() == 0 {
+// 					fmt.Println(p)
+					fmt.Fprintf(os.Stderr, "[empty file]" + p + "\n")
+				} else {
+					fmt.Fprintf(os.Stderr, "[not empty file] " + p + "\n")
+				}
 			}
 		}
 	}
