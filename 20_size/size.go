8aaf8354c81a04ac1aa96478dd5d16a301033152
Sridhar Sarnobat <ss401533@gmail.com>
2023-07-26 18:11:47 -0700

    Minor changes (untested)

--- 20_size/size.go
+++ 20_size/size.go
@@ -48,58 +48,7 @@ func main() {
 
 
 	upper, _ := humanSizeToBytes("100G")
-	lower, _ := humanSizeToBytes("1")
-	err := 0
-	for i := 1; i < len(os.Args); i++ {
-		fmt.Println("Argument", i, ":", os.Args[i])
-		if strings.HasPrefix(os.Args[i], "+") {
-// 			n, err := strconv.Atoi(os.Args[i])
-			lower, err = humanSizeToBytes(os.Args[i])
-			if err == 0 {
-				fmt.Println("lower bound: ", lower)
-			} else {
-				fmt.Println("Error 1")
-				return;
-			}
-		} else if strings.HasPrefix(os.Args[1], "-") {
 
-			upper, err = humanSizeToBytes(os.Args[i])
-			if err == 0 {
-				fmt.Println("upper bound: ", upper)
-			} else {
-				fmt.Println("Error 2")
-				return;
-			}
-		}
-	}
-	
-	in := bufio.NewReader(os.Stdin)
-	for {
-		s, err := in.ReadString('\n')
-		if err != nil {
-			if err != io.EOF {
-				log.Fatal(err)
-			}
-			break
-		}
-
-		p := strings.TrimSpace(s)
-
-		if _, err := os.Stat(p); !os.IsNotExist(err) {
-
-			switch fileInfo, err := os.Stat(p); {
-			case err != nil:
-				fmt.Println(err)
-			case fileInfo.Size() > lower && fileInfo.Size() < upper:
-				fmt.Printf("File %s is in range\n", s, fileInfo.Size())
-			case fileInfo.Size() > 10*1024*1024:
-// 				fmt.Printf("File %s is bigger than 10MB (%d bytes)\n", filePath, fileInfo.Size())
-
-			default:
-// 				fmt.Println(p)
-			}
-		}
-	}
 }
 
 func humanSizeToBytes(sizeStr string) (int64, error) {
