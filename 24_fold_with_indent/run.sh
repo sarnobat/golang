ec88ede54f5fd3da02d9e50db573c2462b1f393e
Sridhar Sarnobat <sridhar.sarnobat@stanford.edu>
2024-04-01 10:59:43 -0700

    Minor changes (untested)

--- 24_fold_with_indent/run.sh
+++ 24_fold_with_indent/run.sh
@@ -1,1 +1,17 @@
-I don't know why the builtin unix fold doesn't have this. It would be good for printing code that has long lines
+#I don't know why the builtin unix fold doesn't have this. It would be good for printing code that has long lines
+set -e
+
+NAME=myprog
+
+env GOOS=windows GOARCH=amd64 go build main.go
+mv main.exe $NAME.exe
+
+env GOOS=linux GOARCH=amd64 go build main.go
+mv main $NAME.linux
+
+env GOOS=darwin GOARCH=amd64 go build main.go
+cp main $NAME.mac.intel
+
+env GOOS=darwin GOARCH=arm64 go build main.go
+cp main $NAME.mac.m1
+
