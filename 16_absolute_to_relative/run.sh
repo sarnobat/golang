a21a3d085793f33ea4f28eb45586ac83320124ce
Sridhar Sarnobat <sridhar.sarnobat@stanford.edu>
2024-04-05 15:47:47 -0700

    Minor changes (untested)

--- 16_absolute_to_relative/run.sh
+++ 16_absolute_to_relative/run.sh
@@ -22,9 +22,10 @@ mv $PROGRAM_NAME $PROGRAM_NAME.mac.intel
 GOOS=windows GOARCH=amd64 go build $PROGRAM_NAME.go
 mv ${PROGRAM_NAME}.exe ${PROGRAM_NAME}.exe
 
-rsync -a -v *m1		/Volumes/git/github/binaries/mac.m1/bin
-rsync -a -v *linux 	/Volumes/git/github/binaries/linux/bin
-rsync -a -v *intel 	/Volumes/git/github/binaries/mac.intel/bin
+rsync -a -v *m1					~/github/binaries/mac.m1/bin
+rsync -a -v *linux 				~/github/binaries/linux/bin
+rsync -a -v *intel 				~/github/binaries/mac.intel/bin
+rsync -a -v ${PROGRAM_NAME}.exe	~/github/binaries/windows/bin/${PROGRAM_NAME}.exe
 
 rename -f -v 's{.linux$}{}g' /Volumes/git/github/binaries/linux/bin/*.linux
 rename -f -v 's{.mac.intel$}{}g' /Volumes/git/github/binaries/mac.intel/bin/*.intel
