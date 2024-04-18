276a6c47dde979bea623553dec92961bf55765e1
Sridhar Sarnobat <ss401533@gmail.com>
2024-04-17 20:59:21 -0700

    Minor changes (air)

--- 20_size_between/run.sh
+++ 20_size_between/run.sh
@@ -11,6 +11,7 @@ set -o nounset
 
 FILE="size"
 cat <<EOF | tee /tmp/run.sh | \batcat --plain --paging=never --language sh --theme TwoDark
+set -e
 GOOS=linux GOARCH=amd64 go build ${FILE}.go
 mv ${FILE} ${FILE}.linux
 GOOS=darwin GOARCH=arm64 go build ${FILE}.go
@@ -20,7 +21,7 @@ mv ${FILE} ${FILE}.mac.intel
 
 
 rsync -a -v *m1		/Volumes/git/github/binaries/mac.m1/bin
-rsync -a -v *linux 	/Volumes/git/github/binaries/ubuntu/bin
+rsync -a -v *linux 	/Volumes/git/github/binaries/linux/bin
 rsync -a -v *intel 	/Volumes/git/github/binaries/mac.intel/bin
 EOF
 
@@ -29,5 +30,5 @@ cat <<EOF | \batcat --plain --paging=never --language sh --theme TwoDark
 cd /Volumes/git/github/binaries/
 
 To build all:
-sh /tmp/run.sh
+sh -x /tmp/run.sh
 EOF
