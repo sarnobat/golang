53fc18649c8c4d58adad65b15677b8dec0a2e0d6
sarnobat <ss401533@gmail.com>
2024-10-07 12:51:42 -0700

    Update run.sh

--- 23_size_sum/run.sh
+++ 23_size_sum/run.sh
@@ -2,6 +2,8 @@ cat <<EOF
 TODO: add the size of every file supplied on stdin (outputted from find, NOT du)
 
 This is useful if,  .e.g you want to find the sum of all files of type mts
+
+Limitation: doesn't work for small sizes (e.g. total is reported in 0G, 1G or more)
 EOF
 
 env GOOS=linux GOARCH=amd64 go build main.go
