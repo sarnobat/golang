4cfdac6da547bdc4b55253f97a4b641ca0e70762
Sridhar Sarnobat <sridhar.sarnobat@stanford.edu>
2024-11-19 19:21:12 -0800

    Cron daily autocommit

--- 23_size_sum/run.sh
+++ 23_size_sum/run.sh
@@ -1,5 +1,6 @@
 cat <<EOF
-TODO: add the size of every file supplied on stdin (outputted from find, NOT du)
+TODO 2024-10: add the size of every file supplied on stdin (outputted from find, NOT du)
+TODO 2024-10: show running total because it may take time to find the full list of input files.
 
 This is useful if,  .e.g you want to find the sum of all files of type mts
 EOF
