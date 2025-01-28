73a599046ebdc523c7855c5ce8f8d35cfe9898f6
Sridhar Sarnobat <sarnobat@mbp2024.local>
2025-01-27 23:12:38 -0500

    Minor changes (untested) 2025-01-27

--- 26_stemmer_lemmatizer/run.sh
+++ 26_stemmer_lemmatizer/run.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------------
+# DESCRIPTION		
+# DATE				2024
+# AUTHOR			ss401533@gmail.com                                           
+#----------------------------------------------------------------------------
+# template found at ~/.vim/sh_header.temp
+
+set -o errexit
+echo "$0" >> ~/db.git/command_history.txt | ts >> ~/db.git/command_history_timestamped.txt
+
+cat <<EOF | batcat --style=plain --paging=never --language sh --theme TwoDark
 echo "revitalizing" | go run lemmatize.go
+cat /Volumes/git/mwk.git/snippets/*mwk | perl -pe 's{\b}{\n}g' | go run /Volumes/git/github/2024/golang/26_stemmer_lemmatizer/lemmatize.go 2> /dev/null | sort | uniq -c | sort -n
+EOF
+
