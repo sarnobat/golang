94866e58f7e3678735cf04db9a425047a8d4316d
Sridhar Sarnobat <sarnobat@mbp2024.local>
2025-01-27 23:26:21 -0500

    2025-01-27: Minor changes (untested)

--- 27_stemmer_lemmatizer/run.sh
+++ 27_stemmer_lemmatizer/run.sh
@@ -13,6 +13,5 @@ echo "$0" >> ~/db.git/command_history.txt | ts >> ~/db.git/command_history_times
 cat <<EOF | batcat --style=plain --paging=never --language sh --theme TwoDark
 echo "revitalizing" | go run lemmatize.go
 cat /Volumes/git/mwk.git/snippets/*mwk | perl -pe 's{\b}{\n}g' | go run /Volumes/git/github/2024/golang/26_stemmer_lemmatizer/lemmatize.go 2> /dev/null | sort | uniq -c | sort -n
-cat /Volumes/git/mwk.git/snippets/*mwk | perl -pe 's{\b}{\n}g' | /Volumes/git/github/2024/golang/26_stopword_remover/stopwordrm | /Volumes/git/github/2024/golang/27_stemmer_lemmatizer/lemmatize 2> /dev/null | sort | uniq -c | sort -n
-
+cat /Volumes/git/mwk.git/snippets/*mwk | perl -pe 's{\b}{\n}g' | /Volumes/git/github/2024/golang/26_stopword_remover/stopwordrm  2> /dev/null | cat | /Volumes/git/github/2024/golang/27_stemmer_lemmatizer/lemmatize 2> /dev/null
 EOF
