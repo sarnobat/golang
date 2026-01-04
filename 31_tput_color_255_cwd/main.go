64818ea47209c78a807c3ae6d4551255b932bd4b
Sridhar Sarnobat <ss401533@gmail.com>
2026-01-03 21:57:54 -0500

    2026-01-03: cwd2color main.go

--- 31_tput_color_255_cwd/main.go
+++ 31_tput_color_255_cwd/main.go
@@ -13,20 +13,42 @@ func colorByte(s string) uint8 {
 	return uint8(sum[0])
 }
 
+func hashKeyForPath(p string) string {
+	info, err := os.Stat(p)
+	if err != nil {
+		// Path doesn't exist; hash the raw string
+		return p
+	}
+
+	if info.IsDir() {
+		return p
+	}
+
+	// It's a file: use its parent directory
+	dir := filepath.Dir(p)
+	if dir == "." {
+		// relative file in cwd
+		if wd, err := os.Getwd(); err == nil {
+			return wd
+		}
+	}
+	return dir
+}
+
 func main() {
 	fi, err := os.Stdin.Stat()
 	hasStdin := err == nil && (fi.Mode()&os.ModeCharDevice) == 0
 
-	// STDIN MODE: colorize each line independently
+	// STDIN MODE: colorize line-by-line
 	if hasStdin {
 		scanner := bufio.NewScanner(os.Stdin)
-		// allow long lines (find can produce long paths)
 		buf := make([]byte, 0, 64*1024)
 		scanner.Buffer(buf, 1024*1024)
 
 		for scanner.Scan() {
 			line := scanner.Text()
-			c := colorByte(line)
+			key := hashKeyForPath(line)
+			c := colorByte(key)
 			fmt.Printf("\x1b[38;5;%dm%s\x1b[0m\n", c, line)
 		}
 		return
